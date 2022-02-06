79c05819d03e127eb7f80a2045a28c78c86b6801
Kasey <kasey.murdoch@yahoo.com>
2022-02-05 20:24:33 -0800

    Added log of inputs, numbers disable when needed

--- javascript.js
+++ javascript.js
@@ -1,4 +1,5 @@
 const display = document.getElementById(`display`)
+const log = document.getElementById(`log`)
 const zero = document.getElementById(`0`)
 const one = document.getElementById(`1`)
 const two = document.getElementById(`2`)
@@ -22,7 +23,8 @@ const buttons = document.querySelectorAll(`button`)
 console.log(buttons)
 let displayValue = 0
 let storedValue = 0
-let operatorTextText
+let operatorText = ``
+let temp = ``
 
 function addFunc(a, b) {
     return a + b
@@ -37,43 +39,80 @@ function divideFunc(a, b) {
     return a / b
 }
 
+// Iterates through each button and does something depending on which you click on
 buttons.forEach((e) => {
     e.addEventListener('click', () => {
         if (e.textContent < 10){
+            log.textContent += e.textContent
             input(e.textContent)
         }
-        if (e.textContent === `+`) {
-            storedValue = displayValue
-            display.textContent = ``
-            operatorText = `+`
+        // else if (e.textContent === `+`) {
+            
+        //     store(e.textContent)
+        // }
+        // else if (e.textContent === `-`) {
+        //     store(e.textContent)
+        // }
+        // else if (e.textContent === `*`) {
+        //     store(e.textContent)
+        // }
+        // else if (e.textContent === `/`) {
+        //     store(e.textContent)
+        // }
+        else if (e.textContent === `C`) {
+            store(e.textContent)
+            operatorText = ``
+            displayValue = `0`
+            log.textContent = ``
+            enableNum()
         }
-        if (e.textContent === `-`) {
-            storedValue = displayValue
-            display.textContent = ``
-            operatorText = `-`
+        else if (e.textContent === `=`) {
+            // If an operation has already completed. Do nothing
+            if (operatorText === ``) {
+                return
+            }
+            else {
+                // Converts the display strings to numbers then operates with the given operator
+                disableNum()
+                log.textContent += e.textContent
+                storedValue = parseInt(storedValue, 10)
+                displayValue = parseInt(displayValue, 10)
+                operate(operatorText)
+                log.textContent += displayValue
+
+            }
         }
-        if (e.textContent === `*`) {
-            storedValue = displayValue
-            display.textContent = ``
-            operatorText = `*`
-        }
-        if (e.textContent === `/`) {
-            storedValue = displayValue
-            display.textContent = ``
-            operatorText = `/`
-        }
-        if (e.textContent === `C`) {
-            storedValue = displayValue
-            display.textContent = ``
-            operatorText = `C`
-        }
-        if (e.textContent === `=`) {
-            storedValue = parseInt(storedValue, 10)
-            displayValue = parseInt(displayValue, 10)
-            operate(operatorText)
+        else {
+            if (operatorText !== ``) {
+                if (log.textContent.includes(`=`)) {
+                    log.textContent = ``
+                    console.log(log.textContent)
+                }
+                log.textContent += e.textContent
+                temp = operatorText
+                console.log(temp)
+                storedValue = parseInt(storedValue, 10)
+                displayValue = parseInt(displayValue, 10)
+                operate(operatorText)
+                operatorText = temp
+                store(e.textContent)
+                enableNum()
+            }
+            else {
+                log.textContent += e.textContent
+                store(e.textContent)
+                enableNum()
+            }
         }
     })
 })
+
+// Value on screen becomes stored/erased, operator that was clicked is stored
+function store(value) {
+    storedValue = displayValue
+    display.textContent = ``
+    operatorText = value
+}
 
 function input(value) {
     display.textContent += value
@@ -84,6 +123,8 @@ function input(value) {
 function operate(operator) {
     if (operator === `+`) {
         display.textContent = addFunc(storedValue, displayValue)
+        console.log(`After operation storedvalue: ${storedValue}`)
+        console.log(`After operation displayvalue: ${displayValue}`)
     }
     if (operator === `-`) {
         display.textContent = subtractFunc(storedValue, displayValue)
@@ -97,6 +138,35 @@ function operate(operator) {
     if (operator === `C`) {
 
     }
+
+    operatorText = ``
+    storedValue = displayValue
     displayValue = display.textContent
-    console.log(displayValue)
+    console.log(`Ending storedvalue: ${storedValue}`)
+    console.log(`Ending displayvalue: ${displayValue}`)
+}
+
+function disableNum() {
+    one.disabled = true
+    two.disabled = true
+    three.disabled = true
+    four.disabled = true
+    five.disabled = true
+    six.disabled = true
+    seven.disabled = true
+    eight.disabled = true
+    nine.disabled = true
+    zero.disabled = true
+}
+function enableNum() {
+    one.disabled = false
+    two.disabled = false
+    three.disabled = false
+    four.disabled = false
+    five.disabled = false
+    six.disabled = false
+    seven.disabled = false
+    eight.disabled = false
+    zero.disabled = false
+    nine.disabled = false
 }
